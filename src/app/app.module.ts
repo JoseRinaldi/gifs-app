16e06e731c4a41bc93699590bfd3800707e45683
José María Rinaldi <115929308+JoseRinaldi@users.noreply.github.com>
2023-07-03 15:28:08 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
+
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    GifsModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
