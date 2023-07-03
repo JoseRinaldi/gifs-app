a93a83b15618048ff80cc65938b32a7a54716040
José María Rinaldi <115929308+JoseRinaldi@users.noreply.github.com>
2023-07-03 17:23:33 -0300

    creacion de componentes

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GifsHomeComponent } from './pages/gifs-home/gifs-home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GifsHomeComponent,
+    SearchBoxComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    GifsHomeComponent,
+    SearchBoxComponent,
   ]
 })
 export class GifsModule { }
---
+++ src/app/gifs/pages/gifs-home/gifs-home-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './gifs-home-page.component.html',
+})
+export class GifsHomeComponent {
+
+}
