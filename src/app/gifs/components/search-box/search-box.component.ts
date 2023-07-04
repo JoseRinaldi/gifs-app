4605aee03431fbb65aaccf5884c92f1a31c85ef8
José María Rinaldi <115929308+JoseRinaldi@users.noreply.github.com>
2023-07-04 14:10:31 -0300

    tags

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -6,9 +7,33 @@ import { Component } from '@angular/core';
     <h5>Buscar</h5>
     <input type="text"
     class="form-control"
-    placeholder="Buscar gifs...">
+    placeholder="Buscar gifs..."
+    (keyup.enter)="searchTag()"
+    #txtTagInput>
   `
 })
 export class SearchBoxComponent {
 
+  // TODO: Toma una referencia local @ViewChildren toma varias referencias local
+  @ViewChild('txtTagInput')
+  public tagInput! : ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService : GifsService){}
+
+  // searchTag(newTag : string)
+  searchTag(){
+
+    const newTag = this.tagInput.nativeElement.value;
+
+    //console.log({newTag});
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
+
+
+  }
+
 }
+
+// (keyup.enter)="searchTag(txtTagInput.value)"
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _tagHistory : string[] = []
+
+  constructor() { }
+
+  get tagsHistori(){
+
+    return [...this._tagHistory];
+
+  }
+
+  searchTag(tag:string) : void{
+
+    this._tagHistory.unshift(tag);
+
+  }
+
+}
