739ccf5036aa3eff3e9fd6654388c9f380fe13f0
José María Rinaldi <115929308+JoseRinaldi@users.noreply.github.com>
2023-07-05 15:24:26 -0300

    lazy-image gifs-card

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+
+import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
 import { GifsHomeComponent } from './pages/gifs-home/gifs-home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
@@ -10,14 +13,17 @@ import { CardListComponent } from './components/card-list/card-list.component';
   declarations: [
     GifsHomeComponent,
     SearchBoxComponent,
-    CardListComponent
+    CardListComponent,
+    GifsCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports:[
     GifsHomeComponent,
     SearchBoxComponent,
+    GifsCardComponent,
     CardListComponent
   ]
 })
